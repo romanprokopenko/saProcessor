324d8ceece1a55d9789ba95e9ee4bc1907f8cd5b
Roman Prokopenko <prokopenkor5@gmail.com>
2017-10-18 17:29:44 +0300

    finished single search function

--- src/main/java/kpi/diploma/suffixarrayservice/SuffixArrayService.java
+++ src/main/java/kpi/diploma/suffixarrayservice/SuffixArrayService.java
@@ -2,7 +2,9 @@ package kpi.diploma.suffixarrayservice;
 
 import kpi.diploma.suffixarray.SuffixArray;
 import kpi.diploma.suffixarrayservice.request.SingleSubstringSearchRequest;
+import kpi.diploma.suffixarrayservice.response.SingleSubstringSearchResponse;
 import kpi.diploma.suffixarrayservice.response.SuffixArrayServiceResponse;
+import kpi.diploma.suffixarrayservice.util.SingleSubstringSearch;
 
 import java.util.Objects;
 
@@ -10,11 +12,16 @@ public class SuffixArrayService {
 
     public SuffixArrayServiceResponse singleSubstringSearch(SingleSubstringSearchRequest request) {
         Objects.requireNonNull(request);
+        Objects.requireNonNull(request.getSourceText());
+        Objects.requireNonNull(request.getSearchedText());
 
         SuffixArray suffixArray = new SuffixArray(request.getSourceText());
+        SingleSubstringSearch searchProcess = new SingleSubstringSearch(suffixArray);
+        SingleSubstringSearch.SearchResult searchResult = searchProcess.search(request.getSearchedText());
 
-        return null;
+        return new SingleSubstringSearchResponse(searchResult.constructOccurencesList());
     }
+
 
     
 }
--- src/main/java/kpi/diploma/suffixarrayservice/util/SingleSubstringSearch.java
+++ src/main/java/kpi/diploma/suffixarrayservice/util/SingleSubstringSearch.java
@@ -2,9 +2,15 @@ package kpi.diploma.suffixarrayservice.util;
 
 import kpi.diploma.suffixarray.SuffixArray;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class SingleSubstringSearch {
+
+    public static final String STRING_NOT_FOUND = "String not found";
+    public static final String START_INDEX_SHOULD_BE_LESS_THAN_ZERO = "start index should be less than zero";
+    public static final String SEARCHED_STRING_SHOULD_NOT_BE_EMPTY = "Searched string should not be empty";
 
     private SuffixArray suffixArray;
 
@@ -16,15 +22,21 @@ public class SingleSubstringSearch {
     public SearchResult search(String searchedString) {
         Objects.requireNonNull(searchedString);
         if (searchedString.isEmpty()) {
-            throw new IllegalArgumentException("searched string should not be empty");
+            throw new IllegalArgumentException(SEARCHED_STRING_SHOULD_NOT_BE_EMPTY);
         }
 
         if (!isSearchedStringSuitable(searchedString)) {
-            return new SearchResult();
+            throw new SearchException(STRING_NOT_FOUND);
         }
 
         Integer leftBoundIndex = this.searchLeftBoundIndex(searchedString);
         Integer rightBoundIndex = this.searchRightBoundIndex(searchedString, leftBoundIndex);
+
+        if (leftBoundIndex.equals(rightBoundIndex) &&
+                !getSuffixAt(leftBoundIndex).startsWith(searchedString)){
+
+            throw new SearchException(STRING_NOT_FOUND);
+        }
 
         return new SearchResult(leftBoundIndex, rightBoundIndex);
     }
@@ -32,8 +44,8 @@ public class SingleSubstringSearch {
     private Boolean isSearchedStringSuitable(String searchedString) {
         int startIndex = 0;
         int endIndex = this.suffixArray.getSuffixArray().size() - 1;
-        return (searchedString.compareTo(this.getSuffixAt(startIndex)) < 0) ||
-                (searchedString.compareTo(this.getSuffixAt(endIndex))  > 0);
+        return (searchedString.compareTo(this.getSuffixAt(startIndex)) >= 0) &&
+                (searchedString.compareTo(this.getSuffixAt(endIndex)) <= 0);
     }
 
     private Integer searchLeftBoundIndex(String searchedString) {
@@ -57,7 +69,7 @@ public class SingleSubstringSearch {
         int rightIndex = this.suffixArray.getSourceText().length();
         while (leftIndex < rightIndex) {
             int mid = (leftIndex + rightIndex) / 2;
-            if (searchedString.compareTo(this.getSuffixAt(mid)) < 0) {
+            if (searchedString.compareTo(this.getSuffixAt(mid, searchedString.length())) < 0) {
                 rightIndex = mid;
             } else {
                 leftIndex = mid + 1;
@@ -71,28 +83,39 @@ public class SingleSubstringSearch {
         return this.suffixArray.getSourceText().substring(suffixArrayPointer);
     }
 
+    private String getSuffixAt(Integer index, int suffixSize) {
+        int suffixArrayPointer = this.suffixArray.getSuffixArray().get(index);
+        String suffix = this.suffixArray.getSourceText().substring(suffixArrayPointer);
+        if (suffix.length() > suffixSize) {
+            suffix = suffix.substring(0, suffixSize);
+        }
+        return suffix;
+    }
+
     public class SearchResult {
         private Integer startIndex;
         private Integer endIndex;
-
-        public SearchResult() {
-            this.startIndex = -1;
-            this.endIndex = -1;
-        }
 
         public SearchResult(Integer startIndex, Integer endIndex) {
             Objects.requireNonNull(startIndex);
             Objects.requireNonNull(endIndex);
             if (startIndex > endIndex) {
-                throw new IllegalArgumentException("start index should be less than zero");
+                throw new IllegalArgumentException(START_INDEX_SHOULD_BE_LESS_THAN_ZERO);
+            } else if (endIndex - startIndex <= 0) {
+                throw new SearchException(STRING_NOT_FOUND);
             }
 
             this.startIndex = startIndex;
             this.endIndex = endIndex;
         }
 
-        public boolean isStringFound() {
-            return startIndex < 0 || endIndex < 0;
+        public List<Integer> constructOccurencesList() {
+            List<Integer> occurences = new ArrayList<>();
+            for (int i = this.startIndex; i < this.endIndex; i++) {
+                occurences.add(suffixArray.getSuffixArray().get(i));
+            }
+
+            return occurences;
         }
     }
 }
