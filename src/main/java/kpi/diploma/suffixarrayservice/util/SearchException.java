e6f09a74ce3869778f14046060fa5be52e1d78d1
Roman Prokopenko <prokopenkor5@gmail.com>
2017-10-18 14:54:16 +0300

    added searchException

---
+++ src/main/java/kpi/diploma/suffixarrayservice/util/SearchException.java
@@ -0,0 +1,8 @@
+package kpi.diploma.suffixarrayservice.util;
+
+public class SearchException extends RuntimeException{
+
+    public SearchException(String message) {
+        super(message);
+    }
+}
