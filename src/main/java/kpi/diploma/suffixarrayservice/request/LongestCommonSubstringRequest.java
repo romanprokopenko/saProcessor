b79b4d39c1a537284d02fd5d3d9d3a22dd3940a9
Roman Prokopenko <prokopenkor5@gmail.com>
2017-10-22 12:11:48 +0300

    created LongestCommonSubstringRequest

--- src/main/java/kpi/diploma/suffixarrayservice/request/LongestCommonSubstringRequest.java
+++ src/main/java/kpi/diploma/suffixarrayservice/request/LongestCommonSubstringRequest.java
@@ -1,4 +1,30 @@
 package kpi.diploma.suffixarrayservice.request;
 
+import java.util.Objects;
+
 public class LongestCommonSubstringRequest {
+
+    private String firstString;
+    private String secondString;
+
+    public String getFirstString() {
+        return firstString;
+    }
+
+    public void setFirstString(String firstString) {
+        this.firstString = firstString;
+    }
+
+    public String getSecondString() {
+        return secondString;
+    }
+
+    public void setSecondString(String secondString) {
+        this.secondString = secondString;
+    }
+
+    public LongestCommonSubstringRequest(String firstString, String secondString) {
+        this.firstString = Objects.requireNonNull(firstString);
+        this.secondString = Objects.requireNonNull(secondString);
+    }
 }
