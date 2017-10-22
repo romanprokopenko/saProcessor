1409fcae14e6d09f1f764904a17a85eff2416856
Roman Prokopenko <prokopenkor5@gmail.com>
2017-10-22 13:34:14 +0300

    added lcs search method to arrayservice

--- src/main/java/kpi/diploma/App.java
+++ src/main/java/kpi/diploma/App.java
@@ -1,16 +1,30 @@
 package kpi.diploma;
 
-import kpi.diploma.suffixarrayservice.SuffixArrayService;
-import kpi.diploma.suffixarrayservice.request.SingleSubstringSearchRequest;
-import kpi.diploma.suffixarrayservice.response.SuffixArrayServiceResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
 
     public static void main(String[] arg) {
 
-        SingleSubstringSearchRequest request = new SingleSubstringSearchRequest("banana", "e");
-        SuffixArrayService service = new SuffixArrayService();
-        SuffixArrayServiceResponse response = service.singleSubstringSearch(request);
-        System.out.println(response);
+//        String input = "abcabc\u001Fbc";
+//        SuffixArray suffixArray = new SuffixArray(input);
+//        for (int i = 0; i < suffixArray.getSuffixArray().size(); i++) {
+//            System.out.println("-----------------");
+//            System.out.println(suffixArray.getSuffixArray().get(i));
+//            System.out.println("lcp: " + suffixArray.getLcpArray().get(i));
+//            System.out.println(input.substring(suffixArray.getSuffixArray().get(i)));
+//        }
+//        LongestCommonSubstringSearchRequest request = new LongestCommonSubstringSearchRequest("привет петуч", "петучила");
+//        SuffixArrayService service = new SuffixArrayService();
+//        SuffixArrayServiceResponse response = service.longestCommonSubstringSearch(request);
+//        System.out.println(response);
+
+//        SingleSubstringSearchRequest request = new SingleSubstringSearchRequest("banana", "e");
+//        SuffixArrayService service = new SuffixArrayService();
+//        SuffixArrayServiceResponse response = service.singleSubstringSearch(request);
+//        System.out.println(response);
+        SpringApplication.run(App.class, arg);
     }
 }
