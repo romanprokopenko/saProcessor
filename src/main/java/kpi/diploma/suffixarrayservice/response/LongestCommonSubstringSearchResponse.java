4bce4a20cf171f936cbf7b62ac4ff87c76520b54
Roman Prokopenko <prokopenkor5@gmail.com>
2017-10-22 13:33:12 +0300

    added lcs response implementation

--- src/main/java/kpi/diploma/suffixarrayservice/response/LongestCommonSubstringSearchResponse.java
+++ src/main/java/kpi/diploma/suffixarrayservice/response/LongestCommonSubstringSearchResponse.java
@@ -1,4 +1,35 @@
 package kpi.diploma.suffixarrayservice.response;
 
-public class LongestCommonSubstringSearchResponse {
+public class LongestCommonSubstringSearchResponse implements SuffixArrayServiceResponse {
+
+    private String substring;
+    private Integer startIndex;
+    private Integer length;
+
+    public String getSubstring() {
+        return substring;
+    }
+
+    public Integer getStartIndex() {
+        return startIndex;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public LongestCommonSubstringSearchResponse(String substring, Integer startIndex, Integer length) {
+        this.substring = substring;
+        this.startIndex = startIndex;
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "LongestCommonSubstringSearchResponse{" +
+                "substring='" + substring + '\'' +
+                ", startIndex=" + startIndex +
+                ", length=" + length +
+                '}';
+    }
 }
