b5899e093397c416a107a1fb885eb8be56e7c744
Roman Prokopenko <prokopenkor5@gmail.com>
2017-10-22 13:33:49 +0300

    added lcs search method to arrayservice

--- src/main/java/kpi/diploma/suffixarrayservice/SuffixArrayService.java
+++ src/main/java/kpi/diploma/suffixarrayservice/SuffixArrayService.java
@@ -1,9 +1,12 @@
 package kpi.diploma.suffixarrayservice;
 
 import kpi.diploma.suffixarray.SuffixArray;
+import kpi.diploma.suffixarrayservice.request.LongestCommonSubstringSearchRequest;
 import kpi.diploma.suffixarrayservice.request.SingleSubstringSearchRequest;
+import kpi.diploma.suffixarrayservice.response.LongestCommonSubstringSearchResponse;
 import kpi.diploma.suffixarrayservice.response.SingleSubstringSearchResponse;
 import kpi.diploma.suffixarrayservice.response.SuffixArrayServiceResponse;
+import kpi.diploma.suffixarrayservice.util.LongestCommonSubstringSearch;
 import kpi.diploma.suffixarrayservice.util.SingleSubstringSearch;
 
 import java.util.Objects;
@@ -22,6 +25,17 @@ public class SuffixArrayService {
         return new SingleSubstringSearchResponse(searchResult.constructOccurencesList());
     }
 
+    public SuffixArrayServiceResponse longestCommonSubstringSearch(LongestCommonSubstringSearchRequest request) {
+        Objects.requireNonNull(request);
+        Objects.requireNonNull(request.getFirstString());
+        Objects.requireNonNull(request.getSecondString());
 
-    
+        SuffixArray suffixArray = new SuffixArray(request.getUnitedStrings());
+        LongestCommonSubstringSearch searchProcess = new LongestCommonSubstringSearch(suffixArray);
+        LongestCommonSubstringSearch.SearchResult searchResult = searchProcess.search();
+
+        return new LongestCommonSubstringSearchResponse(searchResult.getSubstring(), searchResult.getStartIndex(),
+                searchResult.getLength());
+    }
+
 }
